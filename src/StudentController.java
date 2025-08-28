1dac08fc3267bf7f2e3b1bdf6a0ad52ba2ca57cd
gabrielparim-stack <gabriel.pari.m@tecsup.edu.pe>
2025-08-27 21:01:31 -0500

    Actualizacion del controlador

--- src/StudentController.java
+++ src/StudentController.java
@@ -1,7 +1,5 @@
 public class StudentController {
-
-    public void controller() {
-        //TO DO
+    public static void main(String[] args) {
+        System.out.println("Hello from StudentController");
     }
-
 }
