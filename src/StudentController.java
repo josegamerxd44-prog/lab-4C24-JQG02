acdeea2d818b970be5344932fd13d8999b66f830
Jose Alonso <josegamerxd44@gmail.com>
2025-08-27 21:01:35 -0500

    Agrego método showStudent en StudentController

--- src/StudentController.java
+++ src/StudentController.java
@@ -1,7 +1,13 @@
 public class StudentController {
 
     public void controller() {
-        //TO DO
+        // Método de control de estudiantes
+        System.out.println("Controlando estudiantes...");
+    }
+
+    // Nuevo método
+    public void showStudent(String name) {
+        System.out.println("Mostrando información del estudiante: " + name);
     }
 
 }
