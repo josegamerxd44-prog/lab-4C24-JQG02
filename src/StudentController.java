26ca63063ac7b28dc5ab6f945c6c0d3c961834f6
Jose Alonso <josegamerxd44@gmail.com>
2025-08-28 21:47:24 -0500

    Actualización en mi clase StudentController

--- src/StudentController.java
+++ src/StudentController.java
@@ -3,7 +3,7 @@ public class StudentController {
         // Método de control de estudiantes
         System.out.println("Controlando estudiantes...");
     }
-
+//otros cambios se realizaron 
     // Nuevo método
     public void showStudent(String name) {
         System.out.println("Mostrando información del estudiante: " + name);
